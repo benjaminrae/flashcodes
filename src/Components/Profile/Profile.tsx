1adf829940d910e817a2ed9dda449ea54f991f9a
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-09-11 18:45:11 +0200

    set up basic user profile page

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,7 +1,32 @@
 import "./Profile.css";
+import { useAuth } from "../../contexts/AuthContext";
+import LogoutButton from "../LogoutButton/LogoutButton";
+import { json } from "stream/consumers";
 
 const Profile = () => {
-    return <div className="profile"></div>;
+    const { currentUser } = useAuth();
+    return (
+        <div className="profile">
+            <div className="profile__card">
+                <h2 className="profile__title">My Profile</h2>
+                <div className="profile__group">
+                    <h3 className="profile__subtitle">Name:</h3>
+                    <div>{currentUser.name}</div>
+                </div>
+                <div className="profile__group">
+                    <h3 className="profile__subtitle">Email:</h3>
+                    <div>{currentUser.email}</div>
+                </div>
+                <div className="profile__button-container">
+                    <button>Update Profile</button>
+                    <LogoutButton />
+                </div>
+            </div>
+            <div className="profile__sets">
+                <div className="profile__title">My Sets</div>
+            </div>
+        </div>
+    );
 };
 
 export default Profile;
