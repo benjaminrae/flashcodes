1316fb55a8201dc15f72714afe81ed115017fd28
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-09-11 04:36:41 +0200

    add basic header and styles

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Signup from "../Signup/Signup";
-
+import Header from "../Header/Header";
 import "./App.css";
 
 function App() {
     return (
         <div className="app">
+            <Header />
             <Signup />
         </div>
     );
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <header>
+            <h1 className="header__title">Flashcode</h1>
+        </header>
+    );
+};
+
+export default Header;
