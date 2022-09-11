2b715b41af98a9f8ce41b2fea85334bc3c3c0b3b
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-09-11 03:59:44 +0200

    initialise Signup component

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Signup from "../Signup/Signup";
 
 import "./App.css";
 
 function App() {
-    return <div className="app"></div>;
+    return (
+        <div className="app">
+            <Signup />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/Signup/Signup.tsx
@@ -0,0 +1,7 @@
+import "./Signup.css";
+
+const Signup = () => {
+    return <>Sign UP</>;
+};
+
+export default Signup;
