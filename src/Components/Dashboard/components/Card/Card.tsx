e9df3125635514682d0d7193e908cb25b45fd821
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-09-12 14:37:49 +0200

    updated styling of cards and their containers

--- src/Components/Dashboard/components/Card/Card.tsx
+++ src/Components/Dashboard/components/Card/Card.tsx
@@ -8,11 +8,13 @@ type CardProps = {
 const Card = ({ index, coverImageUrl, title }: CardProps) => {
     return (
         <div className="card" key={index}>
-            <img src={coverImageUrl} alt="" className="card__cover-image" />
-            <h3 className="card__title">{title}</h3>
-            <div className="card__bottom">
-                <div className="card__author">User</div>
-                <div className="card__likes">💟0</div>
+            <div className="card__container">
+                <img src={coverImageUrl} alt="" className="card__cover-image" />
+                <h3 className="card__title">{title}</h3>
+                <div className="card__bottom">
+                    <div className="card__author">User</div>
+                    <div className="card__likes">💟0</div>
+                </div>
             </div>
         </div>
     );
