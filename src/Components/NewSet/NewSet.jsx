0755c2ac46c8cbb34538fdf34d5ff120e3ab4338
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-09-12 08:15:29 +0200

    add onChange handlers for NewSet data

--- src/Components/NewSet/NewSet.jsx
+++ src/Components/NewSet/NewSet.jsx
@@ -2,6 +2,11 @@ import "./NewSet.css";
 import { useState } from "react";
 
 const NewSet = () => {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [coverImageURL, setCoverImageURL] = useState("");
+    const [languages, setLanguages] = useState("");
+
     return (
         <div class="new-set">
             <div className="new-set__container">
@@ -10,21 +15,42 @@ const NewSet = () => {
                 </h2>
                 <form className="new-set__form">
                     <div className="form__group">
-                        <label>Title</label>
-                        <input></input>
+                        <label htmlFor="title">Title</label>
+                        <input
+                            name="title"
+                            onChange={(event) => {
+                                setTitle(event.target.value);
+                            }}
+                        ></input>
                     </div>
                     <div className="form__group">
-                        <label>Description</label>
-                        <input></input>
+                        <label htmlFor="description">Description</label>
+                        <input
+                            name="description"
+                            onChange={(event) => {
+                                setDescription(event.target.value);
+                            }}
+                        ></input>
                     </div>
                     <div className="form__group">
-                        <label>Cover image URL</label>
-                        <input></input>
+                        <label htmlFor="cover-image-url">Cover image URL</label>
+                        <input
+                            name="cover-image-url"
+                            onChange={(event) => {
+                                setCoverImageURL(event.target.value);
+                            }}
+                        ></input>
                     </div>
                     <div className="form__group">
-                        <label></label>
-                        <input></input>
+                        <label htmlFor="languages">Languages</label>
+                        <input
+                            name="languages"
+                            onChange={(event) => {
+                                setLanguages(event.target.value);
+                            }}
+                        ></input>
                     </div>
+                    <div className="form__divider"></div>
                 </form>
             </div>
         </div>
